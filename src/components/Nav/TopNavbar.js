c316e8cf54945b137c365d9bfd214017358645f6
Dayun <dy6578ekdbs@gmail.com>
2023-05-08 01:37:07 +0900

    ✨TopNavbar compo

--- src/components/Nav/TopNavbar.js
+++ src/components/Nav/TopNavbar.js
@@ -1,5 +1,12 @@
+import styled from "styled-components";
 const TopNavbar = () => {
-  return <div>상단 네트바</div>;
+  return <NavDiv>상단 네트바</NavDiv>;
 };
 
 export default TopNavbar;
+
+const NavDiv = styled.div`
+  height: 60px;
+  width: 100%;
+  border: 1px solid red;
+`;
