6d3fc7af3991b0011061748a8943668e43fffb2a
jimin song <songing01@gmail.com>
2023-05-07 17:47:55 +0900

    ✅ Add extra data to test scroll up

--- src/components/List/RestaurantList.js
+++ src/components/List/RestaurantList.js
@@ -32,6 +32,35 @@ const RestaurantList = () => {
       dibs: false,
       rating: 4.3,
     },
+    {
+      image:
+        "https://lh3.googleusercontent.com/p/AF1QipNLW-Hz9RV16lviOVvInbfOaK2j568i5MeLSAOS=s1360-w1360-h1020",
+      title: "모미지 식당",
+      location: "신촌",
+      recommendation: "소고기 가지 덮밥",
+      avgPrice: "10000원",
+      dibs: true,
+      rating: 4.5,
+    },
+    {
+      image:
+        "https://lh3.googleusercontent.com/p/AF1QipPdd7dtJ_KrGlanM5drUOWG71RbJW1-WdTcH4tS=s1360-w1360-h1020",
+      title: "사장님 돈까스",
+      location: "신촌",
+      recommendation: "치즈돈까스",
+      avgPrice: "12000원",
+      dibs: false,
+      rating: 4.3,
+    },
+    {
+      image: undefined,
+      title: "사장님 돈까스",
+      location: "신촌",
+      recommendation: "치즈돈까스",
+      avgPrice: "12000원",
+      dibs: false,
+      rating: 4.3,
+    },
   ];
 
   return (
