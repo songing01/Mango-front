516b2f5dd8b10a18caec2b139cc74114fed6b75b
Dayun <dy6578ekdbs@gmail.com>
2023-05-08 01:37:26 +0900

    ✨Mainpage publishing

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,5 +1,52 @@
+import styled from "styled-components";
+// asset
+import logo from "../assets/logo/logo.png";
+// component
+import SearchBox from "../components/Search/SearchBox";
+import TopNavbar from "../components/Nav/TopNavbar";
+import NearbyBtn from "../components/Button/Main/NearbyBtn";
+import MainButtonContainer from "../components/Button/Main/MainButtonContainer";
+import MainSlideBtnContainer from "../components/Button/Main/MainSlideBtnContainer";
+import EditorsBtn from "../components/Button/Main/EditorsBtn";
 const MainPage = () => {
-  return <div>메인 페이지</div>;
+  return (
+    <Div>
+      <TopNavbar />
+
+      <Logo src={logo} />
+
+      <Container>
+        <SearchBox />
+        <NearbyBtn />
+        <MainButtonContainer />
+        <MainSlideBtnContainer />
+        <EditorsBtn />
+      </Container>
+    </Div>
+  );
 };
 
 export default MainPage;
+
+const Div = styled.div`
+  position: relative;
+
+  width: 100%;
+  height: 100%;
+`;
+
+const Logo = styled.img`
+  width: 111px;
+  height: auto;
+
+  position: absolute;
+  top: 9px;
+  left: 16px;
+`;
+
+const Container = styled.div`
+  padding: 16px;
+  box-sizing: border-box;
+  width: 100%;
+  height: auto;
+`;
