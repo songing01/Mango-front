bd7b4731105974118a291998d8b292cef6d57456
jimin song <songing01@gmail.com>
2023-05-07 18:16:37 +0900

    💄 Add props to change the color of bar If the current page is Main, set 'isMain' to true. if the props is true, background color is #f4f4f4.

--- src/components/Bottom/BarUp.js
+++ src/components/Bottom/BarUp.js
@@ -1,12 +1,12 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { ReactComponent as Up } from "../../assets/up_vector.svg";
 
-const BarUp = () => {
+const BarUp = ({ isMain }) => {
   const moveToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
   return (
-    <ScrollUpButton onClick={moveToTop}>
+    <ScrollUpButton isMain={isMain || true} onClick={moveToTop}>
       <Up />
     </ScrollUpButton>
   );
@@ -18,4 +18,10 @@ const ScrollUpButton = styled.button`
   width: 100%;
   height: 32px;
   background: #606060;
+  border: 0;
+  ${props =>
+    props.isMain &&
+    css`
+      background: #f4f4f4;
+    `}
 `;
