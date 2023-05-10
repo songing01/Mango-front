4a36d3bd1135f05cdee39f5a8f964635b6928390
jimin song <songing01@gmail.com>
2023-05-10 13:36:45 +0900

    ✨ Add routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
+import ListPage from "./pages/ListPage";
+import CreateReviewPage from "./pages/CreateReviewPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/list" element={<ListPage />} />
+      <Route path="/create-review" element={<CreateReviewPage />} />
     </Routes>
   );
 }
