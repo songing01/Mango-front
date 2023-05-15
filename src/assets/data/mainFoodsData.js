1f84901f1d4ce7aff9f9876660f0b1cb2379d285
Dayun <dy6578ekdbs@gmail.com>
2023-05-15 20:47:43 +0900

    💄mainFoodButtons

---
+++ src/assets/data/mainFoodsData.js
@@ -0,0 +1,27 @@
+import rice from "../icon/mainbuttonbox/rice.png";
+import pasta from "../icon/mainbuttonbox/pasta.png";
+import chinafood from "../icon/mainbuttonbox/chinafood.png";
+import japan from "../icon/mainbuttonbox/japan.png";
+import mexican from "../icon/mainbuttonbox/mexican.png";
+import noodle from "../icon/mainbuttonbox/noodle.png";
+import kimbap from "../icon/mainbuttonbox/kimbap.png";
+import hamburger from "../icon/mainbuttonbox/hamburger.png";
+import cake from "../icon/mainbuttonbox/cake.png";
+import coffee from "../icon/mainbuttonbox/coffee.png";
+import soju from "../icon/mainbuttonbox/soju.png";
+import vegetable from "../icon/mainbuttonbox/vegetable.png";
+
+export const mainFoodsData = [
+  { id: 1, food: "한식", img: rice },
+  { id: 2, food: "양식", img: pasta },
+  { id: 3, food: "중식", img: chinafood },
+  { id: 4, food: "일식", img: japan },
+  { id: 5, food: "멕시칸", img: mexican },
+  { id: 6, food: "아시안", img: noodle },
+  { id: 7, food: "분식", img: kimbap },
+  { id: 8, food: "패스트푸드", img: hamburger },
+  { id: 9, food: "베이커리 디저트", img: cake },
+  { id: 10, food: "카페", img: coffee },
+  { id: 11, food: "주류", img: soju },
+  { id: 12, food: "채식", img: vegetable },
+];
