ab0c83db9b2a40a352c7d9ad71c8a1c6d54a048f
Dayun <dy6578ekdbs@gmail.com>
2023-05-18 21:42:55 +0900

    ✨default axios instance

--- src/api/client.js
+++ src/api/client.js
@@ -5,12 +5,10 @@ const client = axios.create();
 client.defaults.baseURL = `${SPRING_URL}`; // spring
 client.defaults.withCredentials = true;
 
-const token = localStorage.getItem("token"); // access token
+const token = localStorage.getItem("mangotoken"); // access token
 console.log("현재 토큰", token);
 
-client.defaults.headers.common["Authorization"] = token
-  ? `Bearer ${token}`
-  : null;
+client.defaults.headers.common["Authorization"] = token ? `${token}` : null;
 
 console.log(
   "현재 설정된 토큰",
