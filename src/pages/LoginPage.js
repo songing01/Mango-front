19d3c998a602ce3b9f21e4ef1c75138505a9bad2
Dayun <dy6578ekdbs@gmail.com>
2023-05-22 19:48:18 +0900

    🔥remove useless code

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -15,7 +15,6 @@ const LoginPage = () => {
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const _handleKakaoLogin = () => {
-    console.log("카카오로그인 버튼 클릭");
     window.location.href = KAKAO_AUTH_URL;
   };
 
