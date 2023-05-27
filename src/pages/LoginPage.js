941f949960dd2093775c86ad34580c2795d77fd4
Dayun <dy6578ekdbs@gmail.com>
2023-05-27 19:55:18 +0900

    fix : 오타

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -7,9 +7,12 @@ import KakaoBtn from "../components/Button/KakaoBtn";
 
 const LoginPage = () => {
   const CLIENT_MAIN_URL = process.env.REACT_APP_REACT_URL;
+
   const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
 
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REST_API_KEY}&response_type=code`;
+  const REDIRECT_URI = encodeURI(`${CLIENT_MAIN_URL}/KakaoLogin`);
+
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const _handleKakaoLogin = () => {
     window.location.href = KAKAO_AUTH_URL;
