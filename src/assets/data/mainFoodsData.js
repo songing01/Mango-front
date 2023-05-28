d8a739aa4a24183bcb7c8e4b6e62c386e9e63984
Dayun <dy6578ekdbs@gmail.com>
2023-05-28 12:36:52 +0900

    fix : icon resize

--- src/assets/data/mainFoodsData.js
+++ src/assets/data/mainFoodsData.js
@@ -12,16 +12,16 @@ import soju from "../icon/mainbuttonbox/soju.png";
 import vegetable from "../icon/mainbuttonbox/vegetable.png";
 
 export const mainFoodsData = [
-  { id: 1, food: "한식", img: rice },
-  { id: 2, food: "양식", img: pasta },
-  { id: 3, food: "중식", img: chinafood },
-  { id: 4, food: "일식", img: japan },
-  { id: 5, food: "멕시칸", img: mexican },
-  { id: 6, food: "아시안", img: noodle },
-  { id: 7, food: "분식", img: kimbap },
-  { id: 8, food: "패스트푸드", img: hamburger },
-  { id: 9, food: "베이커리 디저트", img: cake },
-  { id: 10, food: "카페", img: coffee },
-  { id: 11, food: "주류", img: soju },
-  { id: 12, food: "채식", img: vegetable },
+  { id: 1, food: "한식", img: rice, class: "rice" },
+  { id: 2, food: "양식", img: pasta, class: "pasta" },
+  { id: 3, food: "중식", img: chinafood, class: "chinafood" },
+  { id: 4, food: "일식", img: japan, class: "japan" },
+  { id: 5, food: "멕시칸", img: mexican, class: "mexican" },
+  { id: 6, food: "아시안", img: noodle, class: "noodle" },
+  { id: 7, food: "분식", img: kimbap, class: "kimbap" },
+  { id: 8, food: "패스트푸드", img: hamburger, class: "hamburger" },
+  { id: 9, food: "베이커리 디저트", img: cake, class: "cake" },
+  { id: 10, food: "카페", img: coffee, class: "coffee" },
+  { id: 11, food: "주류", img: soju, class: "soju" },
+  { id: 12, food: "채식", img: vegetable, class: "vegetable" },
 ];
